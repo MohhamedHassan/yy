18382d41faa3f71efc91d76c57a56797517f2004
Mohamed Hassan <mohamed>
2022-07-03 08:02:49 +0200

    h

--- src/app/aboutuss/aboutuss.component.ts
+++ src/app/aboutuss/aboutuss.component.ts
@@ -7,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aboutuss.component.scss']
 })
 export class AboutussComponent implements OnInit {
-
+  one
+  two
+  three
   constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get('http://finalmohamed-001-site1.itempurl.com/api/AboutControuller/GetCont').subscribe(
+      (res:any) => {
+        this.one=res?.VolunteerNum
+        this.two=res?.DisabledNum
+        this.three=res?.AskeHelpNum
+      }
+    )
   }
 
 }
